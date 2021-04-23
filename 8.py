bcfdcdf331a744d194d8440207c459c641611f71
Mohammed Owez <mohammedowez359@gmail.com>
2021-04-23 18:37:15 +0530

    added

---
+++ 8.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+arr = list(map(int, input().split()))
+zes = max(arr)
+#print(zes)
+
+i=0
+while(i<n):
+    if zes ==max(arr):
+        arr.remove(max(arr))
+    i+=1
+print(max(arr))
