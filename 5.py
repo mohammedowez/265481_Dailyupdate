7ce51e7b44c1569abe04e8bfe05ee4073fca78af
Mohammed Owez <mohammedowez359@gmail.com>
2021-04-23 18:30:41 +0530

    added print statment

---
+++ 5.py
@@ -0,0 +1,1 @@
+print('{0} \n{1} \n{2}'.format((a + b), (a - b), (a * b)))
---
+++ 7.py
@@ -0,0 +1,7 @@
+def is_leap(year):
+    if year%4==0:
+        return True
+    elif year%100==0:
+        return False
+    elif year%400==0:
+        return True
